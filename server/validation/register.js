d8e4388201b43de4098c5a633a34f28ac7ecaf45
AlexVak <avakarchuk@me.com>
2019-02-19 13:46:57 +0200

    react frontend added

--- server/validation/register.js
+++ server/validation/register.js
@@ -9,12 +9,12 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
-  }
-
   if (!Validator.isLength(data.name, { min: 3, max: 30 })) {
     errors.name = "Name must be between 3 and 30 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
   }
 
   if (Validator.isEmpty(data.email)) {
