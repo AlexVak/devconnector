e265f28e64dd0fb6b996bccb356e202777e86dcc
AlexVak <avakarchuk@me.com>
2019-02-26 16:41:05 +0200

    deploy scripts

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,21 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const store = createStore(rootReducer, initialState, enhancer);
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t alexvak/devconnector-client:latest -t alexvak/devconnector-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alexvak/devconnector-server:latest -t alexvak/devconnector-server:$SHA -f ./server/Dockerfile ./server
+
+docker push alexvak/devconnector-client:latest
+docker push alexvak/devconnector-server:latest
+
+docker push alexvak/devconnector-client:$SHA
+docker push alexvak/devconnector-server:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/devconnector-client-deployment devconnector-client=alexvak/devconnector-client:$SHA
+kubectl set image deployments/devconnector-server-deployment devconnector-server=alexvak/devconnector-server:$SHA
