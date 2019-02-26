e353d4a9dfb2ffd2dba96a4a96f600a91848e2a9
AlexVak <avakarchuk@me.com>
2019-02-26 19:07:28 +0200

    add test route

--- server/server.js
+++ server/server.js
@@ -35,6 +35,10 @@ app.use("/users", users);
 app.use("/profile", profile);
 app.use("/posts", posts);
 
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
